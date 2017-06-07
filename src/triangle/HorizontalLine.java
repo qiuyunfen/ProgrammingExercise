e161fbefbcb34614f80e9a9f18e375d1d34280fe
qiuyunfen <1150382430@qq.com>
2017-06-07 18:23:13 +0800

    draw a right triangle

--- src/triangle/HorizontalLine.java
+++ src/triangle/HorizontalLine.java
@@ -4,7 +4,7 @@ public class HorizontalLine {
     static int n = 8;
 
     public static void main(String[] args) {
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print("*");
         }
     }
--- src/triangle/VerticalLine.java
+++ src/triangle/VerticalLine.java
@@ -4,7 +4,7 @@ public class VerticalLine {
     static int n = 3;
 
     public static void main(String[] args) {
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println("*");
         }
     }
