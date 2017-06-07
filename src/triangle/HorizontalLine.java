7d9cb2fa6482f961c446a49263ed7b0326f47765
qiuyunfen <1150382430@qq.com>
2017-06-07 17:00:25 +0800

    print n asterisks on one line

---
+++ src/triangle/HorizontalLine.java
@@ -0,0 +1,11 @@
+package triangle;
+
+public class HorizontalLine {
+    static int n = 8;
+
+    public static void main(String[] args) {
+        for(int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+}
