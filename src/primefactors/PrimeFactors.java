a04092ba9145f0db5237b0740121eace37b50d7e
qiuyunfen <1150382430@qq.com>
2017-06-07 19:34:11 +0800

    modify prime factors

--- src/primefactors/PrimeFactors.java
+++ src/primefactors/PrimeFactors.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
-    static int n = 30;
+    static int n = 17;
     public static void main(String[] args) {
         List<Integer> factors = generate(n);
         for(int i = 0; i < factors.size(); i++) {
@@ -16,11 +16,18 @@ public class PrimeFactors {
     }
     public static List<Integer> generate(int n) {
         List<Integer> factors = new ArrayList<Integer>();
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i == 0) {
+        for (int i = 2; i <= n/2; i++) {
+            if(n % i == 0 && isPrime(i)) {
                 factors.add(i);
             }
         }
         return factors;
+    }
+
+    public static boolean isPrime(int num) {
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
     }
 }
