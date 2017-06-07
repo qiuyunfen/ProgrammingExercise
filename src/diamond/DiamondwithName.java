6330d26c5fba14d83079d602dd4d2b1a5f06e4fc
qiuyunfen <1150382430@qq.com>
2017-06-07 19:08:36 +0800

    print diamond with name

---
+++ src/diamond/DiamondwithName.java
@@ -0,0 +1,39 @@
+package diamond;
+
+public class DiamondwithName {
+    static int n = 5;
+    static String name = "Bill";
+
+    public static void main(String[] args) {
+        int totalAsteriskNum = 1 + (n - 2) * 2;
+        int maxlength = totalAsteriskNum > name.length() ? totalAsteriskNum : name.length();
+        StringBuffer[] diamonds = new StringBuffer[n];
+
+        for (int i = 1; i < n; i++) {
+            int currentAsteriskNum  = 1 + (i - 1) * 2;
+            int blankNum = (maxlength - currentAsteriskNum) / 2;
+            StringBuffer line = new StringBuffer();
+
+            line.append(loop(blankNum, " "));
+            line.append(loop(currentAsteriskNum, "*"));
+            line.append(loop(blankNum, " "));
+            diamonds[i-1] = line;
+            System.out.println(line);
+        }
+        StringBuffer line = new StringBuffer();
+        line.append(loop((maxlength - name.length()) / 2, " "));
+        line.append(name);
+        line.append(loop((maxlength - name.length()) / 2, " "));
+        System.out.println(line);
+        for(int i = n - 2; i >= 0; i--) {
+            System.out.println(diamonds[i]);
+        }
+    }
+    public static String loop(int count, String signal) {
+        String line = "";
+        for(int i = 0; i < count; i++) {
+            line += signal;
+        }
+        return line;
+    }
+}
