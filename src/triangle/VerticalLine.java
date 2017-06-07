f9d695ec1e9490ec8990bdace2ae06761a54a9f6
qiuyunfen <1150382430@qq.com>
2017-06-07 18:17:19 +0800

    print vertical line

---
+++ src/triangle/VerticalLine.java
@@ -0,0 +1,11 @@
+package triangle;
+
+public class VerticalLine {
+    static int n = 3;
+
+    public static void main(String[] args) {
+        for(int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+}
